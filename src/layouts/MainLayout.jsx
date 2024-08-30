2be6f0af32611d7ae3e474d7e9aa367fe29ea4eb
jenbern91 <jenbern91@gmail.com>
2024-08-30 19:04:18 +0800

    final commit aug 30

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import NavBar from '../components/NavBar';
+import { ImOpt } from 'react-icons/im';
 
 
 const MainLayout = () => {
@@ -8,6 +11,7 @@ const MainLayout = () => {
   <>
     <NavBar />
     <Outlet />
+    <ToastContainer />
   </>
   );
 };
